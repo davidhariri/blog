48eead6457038c9640b2cb6b450584717d44e717
David Hariri <david.hariri@gmail.com>
2016-03-02 22:37:37 -0500

    first version of blog finished

--- app.py
+++ app.py
@@ -16,23 +16,23 @@ def read_time_from_article(article):
 	time = len(words) / 150
 
 	if time < 1:
-		return "Less than a minute"
+		return "Less than a minute to read"
 	elif time > 1 and time < 1.5:
-		return "{} minute".format(int(round(time)))
+		return "{} minute to read".format(int(round(time)))
 	else:
-		return "{} minutes".format(int(round(time)))
+		return "{} minutes to read".format(int(round(time)))
 
 @app.route("/")
 def return_home():
-	return render_template("home.html", posts=Posts.get())
+	return render_template("index.html", posts=Posts.get())
 
 @app.route("/posts/")
 def return_posts():
-	return render_template("all_posts.html", posts=Posts.get())
+	return render_template("posts.html", posts=Posts.get())
 
 @app.route("/posts/<_id>")
 def return_post(_id):
-	return render_template("single_post.html", post=Posts.get(_id))
+	return render_template("post.html", post=Posts.get(_id))
 
 if __name__ == "__main__":
 	app.run(debug=True, port=8000)
